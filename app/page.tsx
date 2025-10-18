5b30a37f3a92b7b3c07e3df7b41918eb25f1b684
VIKRANT  SINGH <vikrantsingh@VIKRANTs-MacBook-Air.local>
2025-10-18 15:04:41 +0530

    fixed Sign-in and Sign-up page with updated authMiddleware to clerkMiddleware migration update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+
+import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Home() {
-
   return (
     <main className="h-screen flex items-center justify-center flex-col gap-4">
-      <div className="flex items-center">
-        <p className="mr-4">Hi</p>
-        <UserButton afterSignOutUrl="/" />
-      </div>
+      <SignedIn>
+        <div className="flex items-center">
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </SignedIn>
     </main>
   );
 }
