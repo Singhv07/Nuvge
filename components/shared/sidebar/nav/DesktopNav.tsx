bb4e54a62fd5049df52a79a072adc47026f87991
VIKRANT  SINGH <vikrantsingh@VIKRANTs-MacBook-Air.local>
2025-10-20 00:13:52 +0530

    Added DesktopNav and Conversations and Friends Section in it

--- components/shared/sidebar/nav/DesktopNav.tsx
+++ components/shared/sidebar/nav/DesktopNav.tsx
@@ -1,22 +1,41 @@
 "use client"
 
 import { useNavigation } from '@/app/hooks/useNavigation'
+import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
+import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip'
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 import React from 'react'
 
 const DesktopNav = () => {
-
-    const paths = useNavigation();
+  const { paths } = useNavigation();
 
   return (
-    <Card className='hidden lg:flex lg:flex-col lg:justify-between lg:items-center lg:h-full lg:w-16 lg:px-2 lg:py-4'>
-        <nav></nav>
-        <div className='flex flex-col items-center gap-4'>
-            <UserButton />
-        </div>
+    <Card className="hidden lg:flex lg:flex-col lg:justify-between lg:items-center lg:h-full lg:w-16 lg:px-2 lg:py-4">
+      <nav>
+        <ul className="flex flex-col items-center gap-4">
+          {paths.map((path, id) => (
+            <li key={id} className="relative">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button asChild size="icon" variant={path.active ? "default" : "outline"}>
+                    <Link href={path.href}>
+                      {path.icon}
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>{path.name}</TooltipContent>
+              </Tooltip>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="flex flex-col items-center gap-4">
+        <UserButton />
+      </div>
     </Card>
-  )
-}
+  );
+};
 
-export default DesktopNav
+export default DesktopNav;
