6ad4a6c666889f81f5a871e4d7d7caf0bad17ce8
VIKRANT  SINGH <vikrantsingh@Mac.lan>
2025-10-23 23:54:08 +0530

    Added accepting friends request functionality -> request.ts and Request.tsx in friends/_components folder

--- app/(root)/friends/_components/Request.tsx
+++ app/(root)/friends/_components/Request.tsx
@@ -19,6 +19,7 @@ type Props = {
 const Request = ({ id, imageUrl, username, email}: Props) => {
 
     const {mutate: denyRequest, pending: denyPending} = useMutationState(api.request.deny)
+    const {mutate: acceptRequest, pending: acceptPending} = useMutationState(api.request.accept)
 
   return (
     <Card className='w-full p-2 flex flex-row item-center justify-between gap-2'>
@@ -39,8 +40,18 @@ const Request = ({ id, imageUrl, username, email}: Props) => {
         <div className='flex items-center gap-2'>
             <Button 
                 size='icon' 
-                disabled={denyPending} 
-                onClick={() => {}}>
+                disabled={denyPending || acceptPending} 
+                onClick={() => {
+                    acceptRequest({id}).then(() => {
+                        toast.success("Friend request accepted")
+                    }).catch((error) => {
+                        toast.error(
+                            error instanceof ConvexError 
+                            ? error.data 
+                            : "Unexpected error occured"
+                        )
+                    })
+                }}>
                     <Check />
             </Button>
             <Button 
